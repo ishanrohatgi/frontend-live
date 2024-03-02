516a6e5d24ce95419c3cc7d07d80e6a2764a70bd
Ishan Rohatgi <ishan.77775@gmail.com>
2024-03-03 01:15:43 +0530

    added browser cashing

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,12 +3,17 @@ import { Controlled} from "react-codemirror2";
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/css/css'
+import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Editor = ({lang, value, mode, onChange}) => {
   const handleChange =(editor, data, value) =>{
-    onChange( value);
+    onChange(value);
+    localStorage.setItem(lang, value);
   }
+  useEffect(()=>{
+      onChange(localStorage.getItem(lang));
+    },[])
   return (
     <div className="editor">
       <div className={"lang-title " + lang}>
